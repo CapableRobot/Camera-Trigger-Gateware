76e25bb7165e1308d5f8dee059cca6757c5772fb
Chris Osterwood <osterwood@gmail.com>
2021-02-01 10:39:36 -0500

    Added license header

--- gateware/registers_patch.py
+++ gateware/registers_patch.py
@@ -1,8 +1,23 @@
+# Copyright 2021 Chris Osterwood for Capable Robot Components
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from types import MethodType
 from tabulate import tabulate
 
 def apply(obj):
     obj.append = MethodType(append, obj)
+    obj.elaborate = MethodType(elaborate, obj)
     obj.display_table = MethodType(display_table, obj)
     obj.display_defines = MethodType(display_defines, obj)
     
@@ -10,7 +25,6 @@ def apply(obj):
 
 def _hex(value):
     return "0x" + format(value, '02X')
-
 
 def elaborate(self, platform):
         m = Module()
